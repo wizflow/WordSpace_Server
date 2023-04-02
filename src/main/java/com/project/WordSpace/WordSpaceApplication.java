6036e2482c453550f51a7eed926e5791fef05e8f
ksj051203 <ksj054703@gmail.com>
2023-04-02 23:53:11 +0900

    Project Setting

---
+++ src/main/java/com/project/WordSpace/WordSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.project.WordSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordSpaceApplication.class, args);
+	}
+
+}
